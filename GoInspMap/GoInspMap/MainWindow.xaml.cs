41a9be7c507c9e638e9e1786c58cff20cb9bb758
rscholte2 <rscholte2@avans.nl>
2015-11-26 00:23:28 +0100

    kek

--- GoInspMap/GoInspMap/MainWindow.xaml.cs
+++ GoInspMap/GoInspMap/MainWindow.xaml.cs
@@ -4,14 +4,12 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
-using System.Windows.Controls;
 using System.Windows.Data;
 using System.Windows.Documents;
 using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
-using System.Windows.Shapes;
+using System.Drawing;
+using GoInspMap.ViewModel;
 
 namespace GoInspMap
 {
@@ -24,5 +22,26 @@ namespace GoInspMap
         {
             InitializeComponent();
         }
+
+        public int x = 0;
+        public int y = 0;
+
+
+        public void mouseClicked(object sender, MouseEventArgs e)
+        {
+            x = System.Windows.Forms.Cursor.Position.X;
+            y = System.Windows.Forms.Cursor.Position.Y - 150;
+
+        
+
+            marker.Margin = new Thickness(x*2, y*2 - 68, 0, 0);
+
+            Console.WriteLine(x *2);
+            Console.WriteLine(y *2);
+
+
+        }
+
+        
     }
 }
